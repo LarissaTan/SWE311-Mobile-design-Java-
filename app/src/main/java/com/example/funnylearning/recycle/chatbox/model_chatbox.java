1a2c49a50a51b1f0258a71a5cbd176016f8bc693
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-11-07 19:22:32 +0800

    add style file and some init recycle view

---
+++ app/src/main/java/com/example/funnylearning/recycle/chatbox/model_chatbox.java
@@ -0,0 +1,12 @@
+package com.example.funnylearning.recycle.chatbox;
+
+public class model_chatbox {
+    String image = new String();
+    String name = new String();
+    String messageTop = new String();
+    String time = new String();
+}
+
+/*
+* linear lyaout
+* */
