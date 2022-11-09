f3957797888a204bac6188dd599581caf39a6bff
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-11-09 11:43:41 +0800

    manifest fix

--- app/src/main/java/com/example/funnylearning/Temp_head.java
+++ app/src/main/java/com/example/funnylearning/Temp_head.java
@@ -6,17 +6,22 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.funnylearning.navigation.ReadingFragment;
+import com.example.funnylearning.navigation.SettingFragment;
 import com.example.funnylearning.temp_head.Message;
 
 public class Temp_head extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("temp_head is working");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp_head);
 
         Message messageFragment = new Message();
-        replacementFragment(messageFragment);
+        SettingFragment settingFragment = new SettingFragment();
+        replacementFragment(settingFragment);
     }
 
     private void replacementFragment(Fragment fragment){
