fe9f897c60818eded6975b4abc66ed1e833628ab
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-11-09 13:50:10 +0800

    title fix

--- app/src/main/java/com/example/funnylearning/Temp_head.java
+++ app/src/main/java/com/example/funnylearning/Temp_head.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.funnylearning.navigation.ReadingFragment;
 import com.example.funnylearning.navigation.SettingFragment;
@@ -13,11 +14,21 @@ import com.example.funnylearning.temp_head.Message;
 
 public class Temp_head extends AppCompatActivity {
 
+    private TextView head_bar_title;
+    public String title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("temp_head is working");
         super.onCreate(savedInstanceState);
+
+        title = "Message";
         setContentView(R.layout.activity_temp_head);
+        /* setTest need to put behind the setContentView!!!!! */
+        head_bar_title = findViewById(R.id.head_bar_title);
+        head_bar_title.setText(title);
+
+
 
         Message messageFragment = new Message();
         SettingFragment settingFragment = new SettingFragment();
