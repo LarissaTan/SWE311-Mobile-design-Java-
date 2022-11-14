9bb761514ece2d94f38064b3373730acc16da2bc
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-11-15 00:29:44 +0800

    test chart

--- app/src/main/java/com/example/funnylearning/navigation/HomeFragment.java
+++ app/src/main/java/com/example/funnylearning/navigation/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.example.funnylearning.navigation;
 
+import static com.github.mikephil.charting.utils.ColorTemplate.MATERIAL_COLORS;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,6 +19,13 @@ import com.example.funnylearning.Homepage;
 import com.example.funnylearning.R;
 import com.example.funnylearning.Temp_head;
 import com.example.funnylearning.onBoarding.onBoarding;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,44 +35,19 @@ import com.example.funnylearning.onBoarding.onBoarding;
 public class HomeFragment extends Fragment {
 
     public static final String EXTRA_NAME = "tag";
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public HomeFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GoalFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     public String tag = "0";
@@ -71,6 +56,34 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        BarChart barChart = view.findViewById(R.id.home_chart);
+
+        ArrayList<BarEntry> visitors = new ArrayList<>();
+        visitors.add(new BarEntry(2014,420));
+        visitors.add(new BarEntry(2015,920));
+        visitors.add(new BarEntry(2016,321));
+        visitors.add(new BarEntry(2017,675));
+        visitors.add(new BarEntry(2018,892));
+        visitors.add(new BarEntry(2019,345));
+        visitors.add(new BarEntry(2020,239));
+
+        BarDataSet barDataSet = new BarDataSet(visitors,"Visitors");
+        //barDataSet.setColor(ColorTemplate.JOYFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
+
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Example");
+        barChart.animateX(2000);
+
+
+
+
 
         Button test,ong,cheh;
         test = view.findViewById(R.id.just_for_test);
