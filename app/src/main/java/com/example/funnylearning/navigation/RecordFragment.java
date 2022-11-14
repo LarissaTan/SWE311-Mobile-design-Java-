731dd745402731780e4a5274a2cd2aa2ff6720d8
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-14 19:00:42 +0800

    Record page updated

---
+++ app/src/main/java/com/example/funnylearning/navigation/RecordFragment.java
@@ -0,0 +1,4 @@
+package com.example.funnylearning.navigation;
+
+public class RecordFragment {
+}
--- app/src/main/java/com/example/funnylearning/recycle/mood/model_mood.java
+++ app/src/main/java/com/example/funnylearning/recycle/mood/model_mood.java
@@ -1,4 +1,9 @@
 package com.example.funnylearning.recycle.mood;
 
 public class model_mood {
+    String name = new String();
+
+    public model_mood(String nameTmp) {
+        name = nameTmp;
+    }
 }
