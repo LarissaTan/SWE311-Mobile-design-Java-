f6f82b2d89374235d8c90edda8db3805fce452e2
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-16 22:20:56 +0800

    Recylclerview for reading and math fragment course added

--- app/src/main/java/com/example/funnylearning/navigation/ReadingFragment.java
+++ app/src/main/java/com/example/funnylearning/navigation/ReadingFragment.java
@@ -3,12 +3,18 @@ package com.example.funnylearning.navigation;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.funnylearning.R;
+import com.example.funnylearning.recycle.readmathcourse.model_readmath_course;
+import com.example.funnylearning.recycle.readmathcourse.adapter_readmath_course;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,9 @@ import com.example.funnylearning.R;
  * create an instance of this fragment.
  */
 public class ReadingFragment extends Fragment {
+
+    private RecyclerView couList;
+    private final ArrayList<model_readmath_course> courseList = new ArrayList<model_readmath_course>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +70,20 @@ public class ReadingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reading, container, false);
+        View view = inflater.inflate(R.layout.fragment_reading, container, false);
+
+        couList = (RecyclerView) view.findViewById(R.id.recyclerview_reading_course);
+
+        courseList.clear();
+        System.out.println("message is working");
+        courseList.add(new model_readmath_course("Free", "Learn the basic words (1)", R.drawable.reading1_image, 1, "8MIN"));
+
+        System.out.println("message is working");
+        couList.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+        adapter_readmath_course customAdapterCourse = new adapter_readmath_course(courseList);
+        System.out.println("layout manager is working");
+        couList.setAdapter(customAdapterCourse);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/funnylearning/recycle/readmathcourse/model_readmath_course.java
@@ -0,0 +1,18 @@
+package com.example.funnylearning.recycle.readmathcourse;
+
+public class model_readmath_course {
+    String type;
+    String title;
+    int drawable;
+    int star;
+    String time;
+
+    public model_readmath_course(String typeTmp, String titleTmp, int drawableTmp, int starTmp, String timeTmp)
+    {
+        type = typeTmp;
+        title = titleTmp;
+        drawable = drawableTmp;
+        star = starTmp;
+        time = timeTmp;
+    }
+}
