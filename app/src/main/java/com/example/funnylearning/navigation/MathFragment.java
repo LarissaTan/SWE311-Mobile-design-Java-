6666559e40dd286c142227a017e27bb344aadf32
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-11-17 15:12:50 +0800

    delete some useless things in code

--- app/src/main/java/com/example/funnylearning/navigation/MathFragment.java
+++ app/src/main/java/com/example/funnylearning/navigation/MathFragment.java
@@ -10,51 +10,21 @@ import android.view.ViewGroup;
 
 import com.example.funnylearning.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MathFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MathFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MathFragment() {
+ public MathFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MathFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MathFragment newInstance(String param1, String param2) {
         MathFragment fragment = new MathFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
