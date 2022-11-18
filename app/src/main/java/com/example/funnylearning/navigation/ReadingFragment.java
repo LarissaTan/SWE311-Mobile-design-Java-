96fe4084396a3095097161093cde2b45d723a075
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-18 16:14:52 +0800

    Recylclerview for reading and math fragment course deleted

--- app/src/main/java/com/example/funnylearning/navigation/ReadingFragment.java
+++ app/src/main/java/com/example/funnylearning/navigation/ReadingFragment.java
@@ -11,16 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.funnylearning.R;
-import com.example.funnylearning.recycle.readmathcourse.model_readmath_course;
-import com.example.funnylearning.recycle.readmathcourse.adapter_readmath_course;
 
 import java.util.ArrayList;
 
 public class ReadingFragment extends Fragment {
-
-    private RecyclerView couList;
-    private final ArrayList<model_readmath_course> courseList = new ArrayList<model_readmath_course>();
-
 
     public ReadingFragment() {
         // Required empty public constructor
@@ -43,18 +37,6 @@ public class ReadingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_reading, container, false);
-
-        couList = (RecyclerView) view.findViewById(R.id.recyclerview_reading_course);
-
-        courseList.clear();
-        System.out.println("message is working");
-        courseList.add(new model_readmath_course("Free", "Learn the basic words (1)", R.drawable.reading1_image, 1, "8MIN"));
-
-        System.out.println("message is working");
-        couList.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
-        adapter_readmath_course customAdapterCourse = new adapter_readmath_course(courseList);
-        System.out.println("layout manager is working");
-        couList.setAdapter(customAdapterCourse);
 
         return view;
     }
