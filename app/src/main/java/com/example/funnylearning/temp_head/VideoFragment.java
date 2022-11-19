8500b01d25b250e64d520c6263c685297547338d
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-19 23:15:07 +0800

    video fragment updated

--- app/src/main/java/com/example/funnylearning/temp_head/VideoFragment.java
+++ app/src/main/java/com/example/funnylearning/temp_head/VideoFragment.java
@@ -2,25 +2,22 @@ package com.example.funnylearning.temp_head;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.MediaController;
-import android.widget.VideoView;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.funnylearning.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.DefaultPlayerUiController;
 
 public class VideoFragment extends Fragment {
-
-    private VideoView videoView;
-
-    String VideoUr1 = "https://www.youtube.com/watch?v=-ou9VvyJNOY";
 
     public VideoFragment() {
         // Required empty public constructor
@@ -37,7 +34,6 @@ public class VideoFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
     }
 
 
@@ -46,6 +42,17 @@ public class VideoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_video, container, false);
+
+        YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer){
+                String videoId = "-ou9VvyJNOY";
+                youTubePlayer.loadVideo(videoId,0);
+            }
+        });
 
         return view;
     }
