a3f4715646b2504b7ebfe06799add9bb36773154
userYT <SWE2009512@xmu.edu.my>
2022-11-21 14:17:56 +0800

    add math level activity

---
+++ app/src/main/java/com/example/funnylearning/MathLevel.java
@@ -0,0 +1,14 @@
+package com.example.funnylearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MathLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_math_level);
+    }
+}
