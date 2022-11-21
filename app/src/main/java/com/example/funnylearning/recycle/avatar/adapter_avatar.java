4c63be2d048ce0ff1ad619fd22f54f7b3e27d79f
userYT <SWE2009512@xmu.edu.my>
2022-11-21 20:00:35 +0800

    add item_avatar

--- app/src/main/java/com/example/funnylearning/recycle/avatar/adapter_avatar.java
+++ app/src/main/java/com/example/funnylearning/recycle/avatar/adapter_avatar.java
@@ -1,4 +1,62 @@
 package com.example.funnylearning.recycle.avatar;
 
-public class adapter_avatar {
+import android.graphics.Color;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.funnylearning.R;
+import com.example.funnylearning.recycle.mood.model_mood;
+
+import java.util.ArrayList;
+
+public class adapter_avatar extends RecyclerView.Adapter<com.example.funnylearning.recycle.mood.adapter_mood.ViewHolder>{//bug
+    private final ArrayList<model_avatar> avatarList;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView item_mood_title;
+        ImageView imageView;
+        CardView cardView;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            item_mood_title  = view.findViewById(R.id.record_mood_text);
+            imageView = view.findViewById(R.id.record_mood_image);
+            cardView = view.findViewById(R.id.record_mood_card);
+        }
+    }
+
+    public adapter_avatar(ArrayList<model_avatar> avatarList) {
+        this.avatarList = avatarList;
+    }
+
+    @NonNull
+    @Override
+    public com.example.funnylearning.recycle.mood.adapter_mood.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_mood, viewGroup, false);
+
+        return new com.example.funnylearning.recycle.mood.adapter_mood.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(com.example.funnylearning.recycle.mood.adapter_mood.ViewHolder viewHolder, final int position) {
+        model_mood list = moodList.get(position);
+
+        viewHolder.item_mood_title.setText(list.name);//bug
+        viewHolder.imageView.setImageResource(list.drawable);
+        viewHolder.cardView.setCardBackgroundColor(Color.parseColor(list.color));
+    }
+
+    @Override
+    public int getItemCount() {
+        return avatarList.size();
+    }
 }
--- app/src/main/java/com/example/funnylearning/temp_head/AvatarFragment.java
+++ app/src/main/java/com/example/funnylearning/temp_head/AvatarFragment.java
@@ -3,12 +3,17 @@ package com.example.funnylearning.temp_head;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.funnylearning.R;
+import com.example.funnylearning.recycle.avatar.model_avatar;
+import com.example.funnylearning.recycle.mood.model_mood;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +22,18 @@ import com.example.funnylearning.R;
  */
 public class AvatarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView optList;
+    private final ArrayList<model_avatar> avatarList = new ArrayList<model_avatar>();
 
     public AvatarFragment() {
+
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AvatarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static AvatarFragment newInstance(String param1, String param2) {
         AvatarFragment fragment = new AvatarFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,10 +41,6 @@ public class AvatarFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +48,7 @@ public class AvatarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_avatar, container, false);
+
+        mdList = (RecyclerView) view.findViewById(R.id.recyclerview_record_mood);
     }
 }
