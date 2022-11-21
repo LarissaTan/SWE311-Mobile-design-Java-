6a7fd403c385ae426021cfca9b44e5caed5efd22
userYT <SWE2009512@xmu.edu.my>
2022-11-21 19:38:13 +0800

    add choose photo activity

---
+++ app/src/main/java/com/example/funnylearning/recycle/avatar/adapter_avatar.java
@@ -0,0 +1,4 @@
+package com.example.funnylearning.recycle.avatar;
+
+public class adapter_avatar {
+}
---
+++ app/src/main/java/com/example/funnylearning/recycle/avatar/model_avatar.java
@@ -0,0 +1,4 @@
+package com.example.funnylearning.recycle.avatar;
+
+public class model_avatar {
+}
