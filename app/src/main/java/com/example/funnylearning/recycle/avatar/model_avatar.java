e9919857dbb5f961f5fc177c071cbd5446d294ac
userYT <SWE2009512@xmu.edu.my>
2022-11-21 19:46:23 +0800

    add AvatarFragment

--- app/src/main/java/com/example/funnylearning/recycle/avatar/model_avatar.java
+++ app/src/main/java/com/example/funnylearning/recycle/avatar/model_avatar.java
@@ -1,4 +1,15 @@
 package com.example.funnylearning.recycle.avatar;
 
 public class model_avatar {
+    String name = new String();
+    String messageTop = new String();
+    String time = new String();
+    String alert_num = new String();
+
+    public model_avatar(String nameTmp, String messageTmp, String timeTmp, String num) {
+        name = nameTmp;
+        messageTop = messageTmp;
+        time = timeTmp;
+        alert_num = num;
+    }
 }
