c33525b505712f688421d153a28ece66cb1b614f
Cheh Shu Ze <swe2009961@xmu.edu.my>
2022-11-22 14:12:40 +0800

    math level and reading level update

---
+++ app/src/main/java/com/example/funnylearning/recycle/math_level/adapter_math_level.java
@@ -0,0 +1,57 @@
+package com.example.funnylearning.recycle.math_level;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.funnylearning.R;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+
+public class adapter_math_level extends RecyclerView.Adapter<adapter_math_level.ViewHolder> {
+
+    Context context;
+    ArrayList<model_math_level> mList;
+    public adapter_math_level(ArrayList<model_math_level> mList) {
+
+        this.context = context;
+        this.mList = mList;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(context).inflate(R.layout.item_math_level,parent,false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        model_math_level list = mList.get(position);
+
+        holder.titleImage.setImageResource(list.titleImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        ShapeableImageView titleImage;
+
+       public ViewHolder(@NonNull View itemView) {
+           super(itemView);
+           titleImage = itemView.findViewById(R.id.title_image);
+       }
+   }
+}
--- app/src/main/java/com/example/funnylearning/register/Math_levelFragment.java
+++ app/src/main/java/com/example/funnylearning/register/Math_levelFragment.java
@@ -2,65 +2,82 @@ package com.example.funnylearning.register;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.funnylearning.R;
+import com.example.funnylearning.recycle.exercise.model_exercise;
+import com.example.funnylearning.recycle.math_level.adapter_math_level;
+import com.example.funnylearning.recycle.math_level.model_math_level;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Math_levelFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class Math_levelFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView m_level;
+    RecyclerView.LayoutManager layoutManager;
+    private ArrayList<model_math_level> mList = new ArrayList<model_math_level>();
+    private int imageResourceID;
 
     public Math_levelFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Math_levelFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static Math_levelFragment newInstance(String param1, String param2) {
+    /*public static Math_levelFragment newInstance(String param1, String param2) {
         Math_levelFragment fragment = new Math_levelFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
+        /*if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        }*/
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_math_level, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+        dataInitialize();
+
+        m_level = view.findViewById(R.id.recyclerView_math_level);
+        m_level.setLayoutManager(new LinearLayoutManager(getContext()));
+        m_level.setHasFixedSize(true);
+        adapter_math_level adapter = new adapter_math_level(mList);
+        m_level.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void dataInitialize() {
+
+        imageResourceID = R.drawable.math_level_sheep;
+
+        mList.add(new model_math_level(imageResourceID));
+
     }
 }
