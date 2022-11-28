e2eefcc2d340cc03e1312242b88bbe054e3337d2
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-29 01:23:54 +0800

    jump button added in login and signup page

--- app/src/main/java/com/example/funnylearning/Signup.java
+++ app/src/main/java/com/example/funnylearning/Signup.java
@@ -8,17 +8,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.funnylearning.Database.UserDao;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class Signup extends AppCompatActivity {
+
+    UserDao userDao = new UserDao(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        TextInputEditText name, email, password, repassword;
+
+        name = (TextInputEditText) findViewById(R.id.signUp_name);
+        email = (TextInputEditText) findViewById(R.id.signUp_email);
+        password = (TextInputEditText) findViewById(R.id.signUp_password);
+        repassword = (TextInputEditText) findViewById(R.id.signUp_repassword);
 
         Button btnSignup;
         btnSignup = findViewById(R.id.btnSignup);
+        Button btnJump;
+        btnJump = findViewById(R.id.signUp_jump);
+
         btnSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(Signup.this, Gender.class);
+                startActivity(it);
+            }
+        });
+
+        btnJump.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent it = new Intent(Signup.this, Gender.class);
                 startActivity(it);
