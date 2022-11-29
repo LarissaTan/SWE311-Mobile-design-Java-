19341949c0188583cb6632e6092d6efd179dc9e4
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-30 00:49:10 +0800

    sign up page done

--- app/src/main/java/com/example/funnylearning/Signup.java
+++ app/src/main/java/com/example/funnylearning/Signup.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,88 +14,62 @@ import android.widget.Toast;
 import com.example.funnylearning.Database.UserDao;
 import com.example.funnylearning.Database.UserDataDao;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup extends AppCompatActivity {
 
     UserDao userDao = new UserDao(this);
     UserDataDao userDataDao = new UserDataDao(this);
 
+    TextInputLayout name, email, password, repassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        TextInputEditText name, email, password, repassword;
-
-        name = findViewById(R.id.signUp_name);
-        email = findViewById(R.id.signUp_email);
-        password = findViewById(R.id.signUp_password);
-        repassword = findViewById(R.id.signUp_repassword);
+        name = findViewById(R.id.signUp_inputlayout_name);
+        email = findViewById(R.id.signUp_inputlayout_email);
+        password = findViewById(R.id.signUp_inputlayout_password);
+        repassword = findViewById(R.id.signUp_inputlayout_repassword);
 
         Button btnSignup;
         btnSignup = findViewById(R.id.btnSignup);
-        Button btnJump;
-        btnJump = findViewById(R.id.signUp_jump);
 
         btnSignup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                String user_name = name.getText().toString();
-                String user_email = email.getText().toString();
-                String user_password = password.getText().toString();
-                String user_repassword = repassword.getText().toString();
+                if (confirmInput()) {
+                    String user_name = name.getEditText().getText().toString();
+                    String user_email = email.getEditText().getText().toString();
+                    String user_password = password.getEditText().getText().toString();
+                    String user_repassword = repassword.getEditText().getText().toString();
 
-                if(user_name.equals("")||user_email.equals("")||user_password.equals("")||user_repassword.equals(""))
-                {
-                    Toast.makeText(Signup.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    if(user_password.equals(user_repassword)){
-                        Boolean checkUser = userDao.checkUserEmail(user_email);
-                        if(checkUser==false)
-                        {
-                            Boolean insert = userDao.insertEmailPassword(user_email, user_password);
-                            if(insert == true) {
-                                Toast.makeText(Signup.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                    Boolean checkUser = userDao.checkUserEmail(user_email);
 
-                                Boolean insertName = userDataDao.insertEmailName(user_email, user_name);
-                                Integer userId = userDataDao.getUserId(user_email);
+                    if (checkUser == false) {
+                        Boolean insert = userDao.insertEmailPassword(user_email, user_password);
+                        if (insert == true) {
+                            Toast.makeText(Signup.this, "Registered successfully", Toast.LENGTH_SHORT).show();
 
-                                if(insertName == true && userId != -1)
-                                {
-                                    Toast.makeText(Signup.this, "User data creating", Toast.LENGTH_SHORT).show();
-                                    Intent it = new Intent(Signup.this, Gender.class);
-                                    it.putExtra("userId",userId);
-                                    startActivity(it);
-                                }
-                                else
-                                {
-                                    Toast.makeText(Signup.this, "User data create fail", Toast.LENGTH_SHORT).show();
-                                }
+                            Boolean insertName = userDataDao.insertEmailName(user_email, user_name);
+                            Integer userId = userDataDao.getUserId(user_email);
+
+                            if (insertName == true && userId != -1) {
+                                Intent it = new Intent(Signup.this, Gender.class);
+                                it.putExtra("userId", userId);
+                                startActivity(it);
+                            } else {
+                                Toast.makeText(Signup.this, "User data create fail", Toast.LENGTH_SHORT).show();
                             }
-                            else {
-                                Toast.makeText(Signup.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                            }
+                        } else {
+                            Toast.makeText(Signup.this, "Registration failed", Toast.LENGTH_SHORT).show();
                         }
-                        else
-                        {
-                            Toast.makeText(Signup.this, "User already exists! Please log in", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    else
-                    {
-                        Toast.makeText(Signup.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                    } else {
+                        email.setError("This email already registered!");
+                        Toast.makeText(Signup.this, "User already exists! Please log in", Toast.LENGTH_SHORT).show();
                     }
                 }
-
-            }
-        });
-
-        btnJump.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent it = new Intent(Signup.this, Gender.class);
-                startActivity(it);
             }
         });
 
@@ -104,5 +80,102 @@ public class Signup extends AppCompatActivity {
                 startActivity(it);
             }
         });
+
+
     }
+
+    private boolean confirmInput(){
+        if(!validate_name() | !validate_email() | !validate_password() | !validate_repassword()){
+            return false;
+        } else
+            return true;
+    }
+
+    private boolean validate_name()
+    {
+        String textName = name.getEditText().getText().toString().trim();
+
+        if(textName.isEmpty()) {
+            name.setError("This field cannot be empty!");
+            return false;
+        } else if(textName.length() < 3) {
+            name.setError("Minimum 3 characters");
+            return false;
+        } else if(textName.length() > 20){
+            name.setError("Maximum 20 characters");
+            return false;
+        } else {
+            name.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validate_email()
+    {
+        String textEmail = email.getEditText().getText().toString().trim();
+
+        if(textEmail.isEmpty()) {
+            email.setError("This field cannot be empty!");
+            return false;
+        } else if(textEmail.length() < 3) {
+            email.setError("Minimum 3 characters");
+            return false;
+        } else if(textEmail.length() > 50){
+            email.setError("Maximum 50 characters");
+            return false;
+        } else if(!isEmailValid(textEmail)){
+            email.setError("Please enter a valid email address");
+            return false;
+        } else {
+            email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean isEmailValid(CharSequence address){
+        return Patterns.EMAIL_ADDRESS.matcher(address).matches();
+    }
+
+    private boolean validate_password()
+    {
+        String textPassword = password.getEditText().getText().toString().trim();
+
+        if(textPassword.isEmpty()) {
+            password.setError("This field cannot be empty!");
+            return false;
+        } else if(textPassword.length() < 3) {
+            password.setError("Minimum 3 characters");
+            return false;
+        } else if(textPassword.length() > 20){
+            password.setError("Maximum 20 characters");
+            return false;
+        } else {
+            password.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validate_repassword()
+    {
+        String textRepassword = repassword.getEditText().getText().toString().trim();
+        String textPassword = password.getEditText().getText().toString().trim();
+
+        if(textRepassword.isEmpty()) {
+            repassword.setError("This field cannot be empty!");
+            return false;
+        } else if(textRepassword.length() < 3) {
+            repassword.setError("Minimum 3 characters");
+            return false;
+        } else if(textRepassword.length() > 20){
+            repassword.setError("Maximum 20 characters");
+            return false;
+        } else if(!textRepassword.equals(textPassword)){
+            repassword.setError("The passwords are not same!");
+            return false;
+        } else {
+            repassword.setError(null);
+            return true;
+        }
+    }
+
 }
