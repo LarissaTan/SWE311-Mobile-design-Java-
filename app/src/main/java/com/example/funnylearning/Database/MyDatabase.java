226e8210af91dc67c10ae3c9ef99999b08b9ed83
Ong Cong Kin <congkinong0112@gmail.com>
2022-12-04 18:48:44 +0800

    database create table query added havent test

--- app/src/main/java/com/example/funnylearning/Database/MyDatabase.java
+++ app/src/main/java/com/example/funnylearning/Database/MyDatabase.java
@@ -17,7 +17,33 @@ public class MyDatabase extends SQLiteOpenHelper {
     public static final String CREATE_USER = "create table if not exists tb_User(email varchar(50) primary key, password varchar(20));";
 
     // store user information
-    public static final String CREATE_USERDATA = "create table if not exists tb_UserData(userId integer primary key AUTOINCREMENT,name varchar(20),email varchar(50), age int, gender bool, foreign key (email) references tb_User(email));";
+    public static final String CREATE_USERDATA = "create table if not exists tb_UserData(userId integer primary key AUTOINCREMENT,name varchar(20),email varchar(50), age int, gender bool, location text, profilePicture int, foreign key (email) references tb_User(email));";
+
+    /* Course and game information table
+    course type has a id -> type id include all the course in the system
+    For example: math, reading (english), [future: science, moral, and other]
+    table related to course will have a type id
+    */
+    public static final String CREATE_COURSETYPE = "create table if not exists tb_CourseType(typeId integer primary key autoincrement, typeName varchar(20), level int);";
+    // course table: store the video link for the child to learn
+    public static final String CREATE_COURSE = "create table if not exists tb_Course(courseId integer primary key autoincrement, courseName varchar(50), typeId integer, videoID varchar(20), view int, foreign key(typeId) references tb_CourseType(typeId));";
+    // course comment: store the comment for the video
+    public static final String CREATE_COURSECOMMENT = "create table if not exists tb_CourseComment(courseId integer, userId integer, comment text, time datetime, foreign key(courseID) references tb_Course(courseId), foreign key(userId) references tb_UserData(userId));";
+    // course like: store the like for each video
+    public static final String CREATE_COURSELIKE = "create table if not exists tb_CourseLike(courseId integer, userId integer, foreign key(courseId) references tb_Course(courseId), foreign key(userId) references tb_UserData(userId));";
+    // game: to evaluate the child master the course or not
+    public static final String CREATE_GAME = "create table if not exists tb_Game(gameId integer primary key autoincrement, gameName varchar(50), typeId integer, foreign key(typeId) references tb_CourseType(typeId));";
+    // user game record: to store the record of the game for each user
+    public static final String CREATE_USERGAMERECORD = "create table if not exists tb_UserGameRecord(date date, userId integer, typeId integer, score int, foreign key(userId) references tb_UserData(userId), foreign key(typeId) references tb_CourseType(typeId));";
+    // user goal: record the goal set by the user
+    public static final String CREATE_USERGOAL = "create table if not exists tb_UserGoal(userId integer, typeId integer, date date, foreign key(userId) references tb_UserData(userId), foreign key(typeId) references tb_CourseType(typeId));";
+    // user level: record the current level of the user (achievement)
+    public static final String CREATE_USERLEVEL = "create table if not exists tb_UserLevel(userId integer, typeId integer, date date, foreign key(userId) references tb_UserData(userId), foreign key(typeId) references tb_CourseType(typeId));";
+
+    // user other data
+    public static final String CREATE_USERDAYRECORD = "create table if not exists tb_UserDayRecord(userId integer, mood varchar(10), activity varchar(10), weather varchar(10), learningTime time, recordDate date, foreign key(userId) references tb_UserData(userId));";
+    // user learning data
+    public static final String CREATE_USERLEARNINGRECORD = "create table if not exists tb_UserLearningData(userId integer, learningTime time, learningDate date, foreign key(userId) references tb_UserData(userId));";
 
     public static final String CREATE_CARTOONDATA = "create table if not exists tb_Cartoon(id int primary key,name varchar(20),level int,url varchar(40),duration varchar(10), image varchar(100))";
     public static final String CREATE_CARTOONDETAILS = "create table if not exists tb_CartoonData(id int primary key,summary varchar(200),key1 varchar(200),key2 varchar(200))";
@@ -29,7 +55,6 @@ public class MyDatabase extends SQLiteOpenHelper {
     public static final String DROP_USERDATA = "drop Table if exists tb_UserData;";
     public static final String DROP_CARTOON = "drop Table if exists tb_Cartoon;";
     public static final String DROP_CARTOONDATA = "drop Table if exists tb_CartoonData;";
-
 
 
     //构造函数
