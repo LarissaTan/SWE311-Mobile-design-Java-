2afedd32c17ce98523aff5609935a6019f8ba7ce
Ong Cong Kin <congkinong0112@gmail.com>
2022-12-04 13:12:02 +0800

    forget password email verify update jump button change to homepage

--- app/src/main/java/com/example/funnylearning/ForgotPswEmail.java
+++ app/src/main/java/com/example/funnylearning/ForgotPswEmail.java
@@ -2,18 +2,27 @@ package com.example.funnylearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.funnylearning.Database.UserDao;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class ForgotPswEmail extends AppCompatActivity {
+
+    UserDao userDao = new UserDao(this);
+    TextInputLayout email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_psw_email);
 
+        email = findViewById(R.id.forgot_psw_email_email);
 
         Button btn;
 
@@ -22,9 +31,47 @@ public class ForgotPswEmail extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                Intent it = new Intent(ForgotPswEmail.this, ForgotPswEnter.class);
-                startActivity(it);
+
+                if(validate_email()){
+                    String user_email = email.getEditText().getText().toString();
+
+                    Boolean checkUser = userDao.checkUserEmail(user_email);
+
+                    if(checkUser == true){
+                        Intent it = new Intent(ForgotPswEmail.this, ForgotPswEnter.class);
+                        startActivity(it);
+                    }else {
+                        email.setError("This email haven't registered!");
+                    }
+
+                }
             }
         });
+    }
+
+    private boolean validate_email()
+    {
+        String textEmail = email.getEditText().getText().toString().trim();
+
+        if(textEmail.isEmpty()) {
+            email.setError("This field cannot be empty!");
+            return false;
+        } else if(textEmail.length() < 3) {
+            email.setError("Minimum 3 characters");
+            return false;
+        } else if(textEmail.length() > 50){
+            email.setError("Maximum 50 characters");
+            return false;
+        } else if(!isEmailValid(textEmail)){
+            email.setError("Please enter a valid email address");
+            return false;
+        } else {
+            email.setError(null);
+            return true;
+        }
+    }
+
+    private boolean isEmailValid(CharSequence address){
+        return Patterns.EMAIL_ADDRESS.matcher(address).matches();
     }
 }
