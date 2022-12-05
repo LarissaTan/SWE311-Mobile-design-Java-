0726d306e58e787a03266c8cf5bf1cef31c6c970
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-12-05 17:04:01 +0800

    maybe bugs

---
+++ app/src/main/java/com/example/funnylearning/Signup_3.java
@@ -0,0 +1,14 @@
+package com.example.funnylearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup3);
+    }
+}
