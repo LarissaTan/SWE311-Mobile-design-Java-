e6e8ed5fa4fd4c94c58eb3cf0a36714cae888d10
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-12-09 13:56:05 +0800

    onclick weather item is done

--- app/src/main/java/com/example/funnylearning/recycle/weather/adapter_weather.java
+++ app/src/main/java/com/example/funnylearning/recycle/weather/adapter_weather.java
@@ -17,15 +17,17 @@ import java.util.ArrayList;
 
 public class adapter_weather extends RecyclerView.Adapter<com.example.funnylearning.recycle.weather.adapter_weather.ViewHolder>{//bug
     private final ArrayList<model_weather> weatherList;
+    private int point = -1;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
+        ImageView imageView, click;
         CardView card;
 
         public ViewHolder(View view) {
             super(view);
             imageView = view.findViewById(R.id.record_weather_image);
             card = view.findViewById(R.id.record_weather_card);
+            click = view.findViewById(R.id.record_weather_onclick);
         }
     }
 
@@ -38,7 +40,26 @@ public class adapter_weather extends RecyclerView.Adapter<com.example.funnylearn
     public adapter_weather.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_record_weather, viewGroup, false);
 
-        return new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+
+        /*
+        添加选中的打勾显示
+         */
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //将点击的位置传出去
+                point = holder.getAdapterPosition();
+                //在点击监听里最好写入setVisibility(View.VISIBLE);这样可以避免效果会闪
+                holder.click.setVisibility(View.VISIBLE);
+                //刷新界面 notify 通知Data 数据set设置Changed变化
+                //在这里运行notifyDataSetChanged 会导致下面的onBindViewHolder 重新加载一遍
+                notifyDataSetChanged();
+            }
+        });
+
+
+        return holder;
     }
 
     @Override
@@ -47,13 +68,23 @@ public class adapter_weather extends RecyclerView.Adapter<com.example.funnylearn
 
         viewHolder.imageView.setImageResource(list.drawable);
 
-        //点击事件
-        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewHolder.card.setCardBackgroundColor(-1777665);
-            }
-        });
+        /*
+        onBindViewHolder 方法可能是在class里for添加了其他视图
+        引入mposition与当前的position判断，判断在点击的位置上显示打勾图片，在其他位置上不显示打勾
+         */
+        if (position == point) {
+            viewHolder.click.setVisibility(View.VISIBLE);
+        } else {
+            viewHolder.click.setVisibility(View.INVISIBLE);
+        }
+
+//        //点击事件
+//        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                viewHolder.card.setCardBackgroundColor(-1777665);
+//            }
+//        });
 
     }
 
