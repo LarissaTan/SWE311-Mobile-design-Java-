c2a81f7c386e3bf7b5772c40798333e3d5a258ac
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-12-10 18:39:06 +0800

    FAQ is added

--- app/src/main/java/com/example/funnylearning/Setting.java
+++ app/src/main/java/com/example/funnylearning/Setting.java
@@ -2,13 +2,34 @@ package com.example.funnylearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.funnylearning.Settings.FAQ;
 
 public class Setting extends AppCompatActivity {
+
+    ImageView faq,help,resetPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        faq = findViewById(R.id.setting_faq);
+        help = findViewById(R.id.setting_help);
+        resetPwd = findViewById(R.id.setting_reset);
+
+        faq.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent it = new Intent(Setting.this, FAQ.class);
+                startActivity(it);
+                //finishAndRemoveTask();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/funnylearning/Settings/FAQ.java
@@ -0,0 +1,16 @@
+package com.example.funnylearning.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.funnylearning.R;
+
+public class FAQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
