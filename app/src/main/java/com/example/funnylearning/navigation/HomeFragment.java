469f08bab19070e3ad06be6a7677a0e2545bf9e3
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2022-12-17 15:34:51 +0800

    home fragment

--- app/src/main/java/com/example/funnylearning/navigation/HomeFragment.java
+++ app/src/main/java/com/example/funnylearning/navigation/HomeFragment.java
@@ -1,14 +1,8 @@
 package com.example.funnylearning.navigation;
 
-import static androidx.databinding.DataBindingUtil.setContentView;
-import static com.github.mikephil.charting.utils.ColorTemplate.MATERIAL_COLORS;
-
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
-import android.content.res.TypedArray;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
@@ -26,41 +20,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.funnylearning.Bean.DayRecordBean;
-import com.example.funnylearning.Bean.model.CourseType;
 import com.example.funnylearning.Bean.model.UserGameRecord;
 import com.example.funnylearning.ChatActivity;
 import com.example.funnylearning.Database.CourseTypeDao;
 import com.example.funnylearning.Database.GameDao;
 import com.example.funnylearning.Database.UserDayRecordDao;
 import com.example.funnylearning.Database.UserGameRecordDao;
-import com.example.funnylearning.EnterPage;
 import com.example.funnylearning.Homepage;
 import com.example.funnylearning.R;
-import com.example.funnylearning.Signup_2;
-import com.example.funnylearning.Signup_3;
 import com.example.funnylearning.Temp_head;
-import com.example.funnylearning.onBoarding.onBoarding;
 import com.example.funnylearning.others.ColumnView;
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.mikhaellopez.circularprogressbar.CircularProgressBar;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 
-public class HomeFragment extends Fragment implements OnChartValueSelectedListener {
+public class HomeFragment extends Fragment{
 
     private LinearLayout column;
     private RelativeLayout studytime;
@@ -332,16 +307,6 @@ public class HomeFragment extends Fragment implements OnChartValueSelectedListen
     }
 
     @Override
-    public void onValueSelected(Entry e, Highlight h) {
-
-    }
-
-    @Override
-    public void onNothingSelected() {
-
-    }
-
-    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
@@ -493,117 +458,3 @@ public class HomeFragment extends Fragment implements OnChartValueSelectedListen
     }
 
 }
-
-        /*
-
-
-        用这个方法没办法去掉坐标轴
-
-        BarChart barChart = view.findViewById(R.id.home_chart);
-
-
-        String hexColor = "F9D150";
-        String hexColor2 = "FEF7E1";
-
-        int[] colorArray = new int[]{ColorTemplate.rgb(hexColor2),ColorTemplate.rgb(hexColor)};
-
-        ArrayList<BarEntry> data1 = new ArrayList<>();
-        data1.add(new BarEntry(2014,new float[]{220,60}));
-        data1.add(new BarEntry(2015,new float[]{220,60}));
-        data1.add(new BarEntry(2016,new float[]{220,60}));
-        data1.add(new BarEntry(2017,new float[]{220,60}));
-        data1.add(new BarEntry(2018,new float[]{220,60}));
-        data1.add(new BarEntry(2019,new float[]{220,60}));
-        data1.add(new BarEntry(2020,new float[]{220,60}));
-
-        ArrayList<BarEntry> data2 = new ArrayList<>();
-        data2.add(new BarEntry(2014,60));
-        data2.add(new BarEntry(2015,60));
-        data2.add(new BarEntry(2016,61));
-        data2.add(new BarEntry(2017,65));
-        data2.add(new BarEntry(2018,62));
-        data2.add(new BarEntry(2019,65));
-        data2.add(new BarEntry(2020,79));
-
-
-
-        BarDataSet barData2Set = new BarDataSet(data2,null);
-
-        BarDataSet barData1Set = new BarDataSet(data1,null);
-        barData1Set.setColor(ColorTemplate.rgb(hexColor2));
-        barData1Set.setValueTextColor(Color.WHITE);
-        barData1Set.setValueTextSize(16f);
-
-        barData2Set.setColor(ColorTemplate.rgb(hexColor));
-        barData2Set.setValueTextColor(Color.WHITE);
-        barData2Set.setValueTextSize(16f);
-        BarData barData = new BarData(barData1Set);
-
-        barChart.getAxisLeft().setDrawAxisLine(false);
-        barChart.getAxisRight().setDrawAxisLine(false);
-        barChart.getAxisLeft().setDrawGridLines(false);
-        barChart.getAxisRight().setDrawGridLines(false);
-
-        Legend legend = barChart.getLegend();
-        legend.setEnabled(true);
-
-        XAxis xAxis = barChart.getXAxis();
-        xAxis.setEnabled(true);
-
-        barChart.setFitBars(true);
-        barChart.setData(barData);
-        barChart.setVisibleXRangeMaximum(15);
-        barChart.setDescription(null);
-        barChart.animateX(0);
-
-
-                test.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                final Dialog dialog = new Dialog(getContext());
-                dialog.setContentView(R.layout.dialog_success);
-                dialog.setTitle("Title...");
-                dialog.getWindow().setDimAmount(0.5f);
-                dialog.getWindow ().setBackgroundDrawable (new ColorDrawable(android.graphics.Color.TRANSPARENT));
-
-                // set the custom dialog components - text, image and button
-                TextView text = (TextView) dialog.findViewById(R.id.dialog_txt);
-                text.setText("You find 2 words !  Good job!");
-
-                Button dialogButton = (Button) dialog.findViewById(R.id.dialog_btn_continue);
-                // if button is clicked, close the custom dialog
-                dialogButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-
-                dialog.show();
-
-                // TODO Auto-generated method stub
-                //tag = "1";
-                Intent it = new Intent(getContext(), onBoarding.class);
-                //it.putExtra(EXTRA_NAME,tag);
-                startActivity(it);
-
-
-                btnSignIn.setOnClickListener {
-                    val dialogBinding = layoutInflater.inflate(R.layout.dialog_login_alert,null)
-
-                    val login_dialog = Dialog(this)
-                    login_dialog.setContentView(dialogBinding)
-                    login_dialog.setCancelable(true)
-                    login_dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                    login_dialog.show()
-
-                    val btn_back = dialogBinding.findViewById<Button>(R.id.btn_login_dialog_back)
-                            btn_back.setOnClickListener {
-                        login_dialog.dismiss()
-                    }
-                }
-
-            }
-                    });
-
-
-*/
